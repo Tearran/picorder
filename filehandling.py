94390c10cd7a5e1e4efc71a4ae7badb6c98d4274
Joseph Turner <Tearran@users.noreply.github.com>
2022-09-30 16:33:21 -0700

    Update filehandling.py
    fix TabError: inconsistent use of tabs and spaces in indentation

--- filehandling.py
+++ filehandling.py
@@ -4,14 +4,14 @@ import time
 class write_values(object):
     
     def __init__(self):
-		pass
+        pass
         #self.d1 = str((time.strftime("%d-%m-%Y")))
         #file = open("log/" + self.d1 + '-loggedvals.csv', 'a')
-
+    
         #file.write("Date, Time, Humidity, Pressure, Temperature, MagX, MagY, MagZ" + "\r")
     
     def logvalues(self, sensors):
-		pass
+        pass
         #self.d1 = str((time.strftime("%d-%m-%Y")))
         #file = open("log/" + self.d1 + '-loggedvals.csv', 'a')
         #self.t = str((time.strftime("%H:%M:%S")))
@@ -22,12 +22,12 @@ class write_values(object):
         #self.x = str(sensors['x'])
         #self.y = str(sensors['y'])
         #self.z = str(sensors['z'])
-##        file.write("/log/" + self.d + "," + self.t + "," + str(sensors['humidity']) + "," + str(sensors['pressure']) + str(sensors['temp']) + "\r")
+        ##        file.write("/log/" + self.d + "," + self.t + "," + str(sensors['humidity']) + "," + str(sensors['pressure']) + str(sensors['temp']) + "\r")
         #file.write( self.d + "," + self.t + "," + self.humid + "," + self.press + "," + self.temp + ","+ self.x + "," + self.y + "," + self.z + "\r")
         #print('logged a value to file!' + "\r")
         #print( "Time: " + self.d + " Date: " + self.t + " Humdity:" + self.humid + " Pressure:" + self.press + " Temperature:" + self.temp + " X:"+ self.x + " Y:" + self.y + " Z:" + self.z + "\r")
         
     def end_file():
-		pass
+        pass
         #file.write("END OF LOG")
     
